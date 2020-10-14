5ed723b4cc191f6be261725b136ff5e40e4c4bc7
Adam <adam.lj.haglund@gmail.com>
2020-10-14 02:01:55 +0200

    add todo

--- apps/manager/src/graphql/schema.ts
+++ apps/manager/src/graphql/schema.ts
@@ -13,5 +13,6 @@ export const createSchema = async (generateSnapshot = true) =>
         : { path: resolve(__dirname, "snapshot.graphql") },
     dateScalarMode: "isoDate",
     resolvers: [resolve(__dirname, "../modules/**/*.resolvers.ts")],
+    // TODO: Clean up container
     container: () => Container.of(Math.random() * Number.MAX_SAFE_INTEGER),
   })
