1d6890ac9ad7a95b165dd68d37ffca357b14fe20
Adam <adam.lj.haglund@gmail.com>
2021-01-03 20:03:35 +0100

    update background packages

--- apps/ui/src/config.ts
+++ apps/ui/src/config.ts
@@ -6,43 +6,38 @@ enum Environment {
   Production = "production",
 }
 
-const baseEnv = envsafe(
-  {
-    NODE_ENV: str({
-      choices: [
-        Environment.Development,
-        Environment.Test,
-        Environment.Production,
-      ],
-      default: Environment.Development,
-      input: process.env.NODE_ENV,
-    }),
-    PORT: port({
-      devDefault: 3100,
-      input: process.env.PORT,
-    }),
+const baseEnv = envsafe({
+  NODE_ENV: str({
+    choices: [
+      Environment.Development,
+      Environment.Test,
+      Environment.Production,
+    ],
+    default: Environment.Development,
+    input: process.env.NODE_ENV,
+  }),
+  PORT: port({
+    devDefault: 3100,
+    input: process.env.PORT,
+  }),
 
-    VUE_APP_ENV: str({
-      choices: [Environment.Development, Environment.Production],
-      default: Environment.Development,
-      input: process.env.VUE_APP_ENV,
-    }),
+  VUE_APP_ENV: str({
+    choices: [Environment.Development, Environment.Production],
+    default: Environment.Development,
+    input: process.env.VUE_APP_ENV,
+  }),
 
-    VUE_APP_CDN_URL: str({
-      default: "https://cdn.anisubs.com/",
-      devDefault: "http://localhost:3000/cdn",
-      input: process.env.VUE_APP_CDN_URL,
-    }),
-    VUE_APP_GRAPHQL_URL: str({
-      default: "https://api.anisubs.com/graphql",
-      devDefault: "http://localhost:3000/graphql",
-      input: process.env.VUE_APP_GRAPHQL_URL,
-    }),
-  },
-  {
-    treatEmptyAsNull: true,
-  },
-)
+  VUE_APP_CDN_URL: str({
+    default: "https://cdn.anisubs.com/",
+    devDefault: "http://localhost:3000/cdn",
+    input: process.env.VUE_APP_CDN_URL,
+  }),
+  VUE_APP_GRAPHQL_URL: str({
+    default: "https://api.anisubs.com/graphql",
+    devDefault: "http://localhost:3000/graphql",
+    input: process.env.VUE_APP_GRAPHQL_URL,
+  }),
+})
 
 export const CONFIG = {
   ...baseEnv,
