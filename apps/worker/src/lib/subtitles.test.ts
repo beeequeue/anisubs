ea150b87da1c3873f4deaf9106213372169e1621
Adam <adam.lj.haglund@gmail.com>
2021-01-11 20:24:38 +0100

    wip add node finders

--- apps/worker/src/lib/subtitles.test.ts
+++ apps/worker/src/lib/subtitles.test.ts
@@ -1,33 +1,85 @@
 import { random } from "faker"
-import { Cue } from "subtitle"
+import { v4 as uuid } from "uuid"
 
-import { createJobs } from "@/lib/subtitles"
+import { findEd, findSimultaneous, findOp, Node } from "@/lib/subtitles"
+
+const ONE_MINUTE = 1000 * 60
+const THREE_AND_HALF_MINUTES = 1000 * 60 * 3.5
 
 const randomWords = () => random.words(Math.ceil(Math.random() * 12))
 
-const generateNodes = (arr: Array<[start: number, end: number]>): Cue[] =>
-  arr.map(([start, end]) => ({ start, end, text: randomWords() }))
+const generateNodes = (arr: Array<[start: number, end: number]>): Node[] =>
+  arr.map(([start, end]) => ({ start, end, text: randomWords(), uuid: uuid() }))
 
-describe("createJobs", () => {
-  describe("findMultipleSimultaneous", () => {
-    test("finds simultaneous subtitles", () => {
-      const nodes = generateNodes([
-        [1_000, 2_000], // 0
-        [3_000, 4_000], // 1 Simultaneous
-        [3_500, 4_000], // 2 ⬆
-        [5_000, 6_000], // 3
-        [10_000, 15_000], // 4 Simultaneous
-        [10_500, 12_000], // 5 ⬆
-        [13_000, 14_500], // 6 ⬆
-        [20_000, 21_000], // 7
-      ])
+describe("findSimultaneous", () => {
+  test("finds simultaneous subtitles", () => {
+    const nodes = generateNodes([
+      [1_000, 2_000], // 0
+      [3_000, 4_000], // 1 Simultaneous
+      [3_500, 4_000], // 2 ⬆
+      [5_000, 6_000], // 3
+      [10_000, 15_000], // 4 Simultaneous
+      [10_500, 12_000], // 5 ⬆
+      [13_000, 14_500], // 6 ⬆
+      [20_000, 21_000], // 7
+    ])
 
-      const { findMultipleSimultaneous } = createJobs(nodes)
+    expect(findSimultaneous(nodes)).toStrictEqual([
+      [nodes[1], nodes[2]],
+      [nodes[4], nodes[5], nodes[6]],
+    ])
+  })
+})
 
-      expect(findMultipleSimultaneous()).toStrictEqual([
-        [nodes[1], nodes[2]],
-        [nodes[4], nodes[5], nodes[6]],
-      ])
-    })
+describe("findOp", () => {
+  test("finds nodes in beginning of show", () => {
+    const nodes = generateNodes([
+      [1_000, 2_000], // 0
+      [3_000, 4_000], // 1
+      [ONE_MINUTE + 1_000, ONE_MINUTE + 2_000], // 2 Inside
+      [ONE_MINUTE + 20_000, ONE_MINUTE + 25_000], // 3 Inside
+      [ONE_MINUTE + 60_000, ONE_MINUTE + 65_000], // 4 Inside
+      [THREE_AND_HALF_MINUTES - 20_000, THREE_AND_HALF_MINUTES - 10_000], // 5 Inside
+      [THREE_AND_HALF_MINUTES + 5_000, THREE_AND_HALF_MINUTES + 6_000], // 6
+      [THREE_AND_HALF_MINUTES + 10_000, THREE_AND_HALF_MINUTES + 15_000], // 7
+      [THREE_AND_HALF_MINUTES + 10_500, THREE_AND_HALF_MINUTES + 12_000], // 8
+      [THREE_AND_HALF_MINUTES + 13_000, THREE_AND_HALF_MINUTES + 14_500], // 9
+      [THREE_AND_HALF_MINUTES + 20_000, THREE_AND_HALF_MINUTES + 21_000], // 10
+    ])
+
+    expect(findOp(nodes)).toStrictEqual([
+      nodes[2],
+      nodes[3],
+      nodes[4],
+      nodes[5],
+    ])
+  })
+})
+
+describe("findEd", () => {
+  test("finds nodes in end of show", () => {
+    const lastMs = 1000 * 60 * 25
+
+    // prettier-ignore
+    const nodes = generateNodes([
+      [lastMs - THREE_AND_HALF_MINUTES - 20_000, lastMs - THREE_AND_HALF_MINUTES - 21_000], // 0
+      [lastMs - THREE_AND_HALF_MINUTES - 13_000, lastMs - THREE_AND_HALF_MINUTES - 14_500], // 1
+      [lastMs - THREE_AND_HALF_MINUTES - 10_500, lastMs - THREE_AND_HALF_MINUTES - 12_000], // 2
+      [lastMs - THREE_AND_HALF_MINUTES - 10_000, lastMs - THREE_AND_HALF_MINUTES - 15_000], // 3
+      [lastMs - THREE_AND_HALF_MINUTES, lastMs - THREE_AND_HALF_MINUTES], // 4
+      [lastMs - THREE_AND_HALF_MINUTES + 20_000, lastMs - THREE_AND_HALF_MINUTES + 10_000], // 5
+      [lastMs - ONE_MINUTE - 60_000, lastMs - ONE_MINUTE - 65_000], // 6
+      [lastMs - ONE_MINUTE - 20_000, lastMs - ONE_MINUTE - 25_000], // 7
+      [lastMs - ONE_MINUTE - 1_000, lastMs - ONE_MINUTE - 2_000], // 8
+      [lastMs - 3_000, lastMs - 4_000], // 9
+      [lastMs - 1_000, lastMs - 2_000], // 10
+    ])
+
+    expect(findEd(nodes)).toStrictEqual([
+      nodes[8],
+      nodes[7],
+      nodes[6],
+      nodes[5],
+    ])
   })
 })
--- apps/worker/src/lib/subtitles.ts
+++ apps/worker/src/lib/subtitles.ts
@@ -1,73 +1,112 @@
 import { createReadStream } from "fs"
 
 import { parse, Cue } from "subtitle"
+import { v4 as uuid } from "uuid"
 
-export const createJobs = (nodes: Cue[]) => {
-  const overlaps = (first: Cue | null, second: Cue | null) => {
-    if (first == null || second == null) return false
-
-    return first.end > second.start
-  }
-
-  const findMultipleSimultaneous = () => {
-    const multiples: Cue[][] = []
-    const multipleIndexes = new Set<number>()
-
-    for (let i = 0; i < nodes.length; i++) {
-      if (multipleIndexes.has(i)) continue
-
-      const current = nodes[i]
-      const overlapping = [current]
-
-      for (let y = i + 1; y < nodes.length; y++) {
-        const next = nodes[y]
-
-        if (overlaps(current, next)) {
-          overlapping.push(next)
-
-          multipleIndexes.add(i)
-          multipleIndexes.add(y)
-        } else {
-          if (overlapping.length > 1) {
-            multiples.push(overlapping)
-          }
-
-          break
-        }
-      }
-    }
-
-    return multiples
-  }
-
-  return {
-    findMultipleSimultaneous,
-  }
+export type Node = Cue & {
+  uuid: string
 }
 
-export const findTimestamps = async (
-  filePath: string,
-): Promise<string[] | null> => {
-  const nodes: Cue[] = []
-  // const timestamps: string[] = []
+const ONE_MINUTE = 1000 * 60
+const THREE_AND_HALF_MINUTES = 1000 * 60 * 3.5
 
-  await new Promise<void>((resolve) =>
+const overlaps = (first: Node | null, second: Node | null) => {
+  if (first == null || second == null) return false
+
+  return first.end > second.start
+}
+
+export const findSimultaneous = (nodes: Node[]): Node[][] => {
+  const multiples: Node[][] = []
+  const multipleIndexes = new Set<number>()
+
+  for (let i = 0; i < nodes.length; i++) {
+    if (multipleIndexes.has(i)) continue
+
+    const current = nodes[i]
+    const overlapping = [current]
+
+    for (let y = i + 1; y < nodes.length; y++) {
+      const next = nodes[y]
+
+      if (overlaps(current, next)) {
+        overlapping.push(next)
+
+        multipleIndexes.add(i)
+        multipleIndexes.add(y)
+      } else {
+        if (overlapping.length > 1) {
+          multiples.push(overlapping)
+        }
+
+        break
+      }
+    }
+  }
+
+  return multiples
+}
+
+export const findOp = (nodes: Node[]): Node[] => {
+  const opNodes: Node[] = []
+
+  for (let i = 0; i < nodes.length; i++) {
+    const current = nodes[i]
+
+    if (current.start > THREE_AND_HALF_MINUTES) break
+
+    if (current.start < ONE_MINUTE) continue
+
+    opNodes.push(current)
+  }
+
+  return opNodes
+}
+
+export const findEd = (nodes: Node[]): Node[] => {
+  const edNodes: Node[] = []
+  const lastNodeEnd = nodes[nodes.length - 1].end
+
+  for (let i = nodes.length - 1; i > 0; i--) {
+    const current = nodes[i]
+
+    if (current.end < lastNodeEnd - THREE_AND_HALF_MINUTES) break
+
+    if (current.end > lastNodeEnd - ONE_MINUTE) continue
+
+    edNodes.push(current)
+  }
+
+  return edNodes
+}
+
+const parseSubtitles = async (filePath: string) =>
+  new Promise<Node[]>((resolve) => {
+    const nodes: Node[] = []
+
     createReadStream(filePath)
       .pipe(parse())
       .on("error", (err) => {
         console.error(`Failed to parse subtitles: ${err.toString()}`)
-        resolve()
+        resolve([])
       })
       .on("data", ({ type, data }) => {
         if (type !== "cue") return
 
-        nodes.push(data)
+        nodes.push({
+          uuid: uuid(),
+          ...data,
+        })
       })
-      .on("finish", () => resolve()),
-  )
+      .on("finish", () => resolve(nodes))
+  })
 
-  const { findMultipleSimultaneous } = createJobs(nodes)
-  findMultipleSimultaneous()
+export const findTimestamps = async (
+  filePath: string,
+): Promise<string[] | null> => {
+  // @ts-ignore
+  const nodes = await parseSubtitles(filePath)
+  // const timestamps: string[] = []
 
   return null
 }
