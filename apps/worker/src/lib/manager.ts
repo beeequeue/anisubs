476a8182c1f668648a5e7321b63f47420cd6b23b
Adam <adam.lj.haglund@gmail.com>
2021-01-17 00:41:47 +0100

    fix worker confirmation

--- apps/worker/src/lib/manager.ts
+++ apps/worker/src/lib/manager.ts
@@ -13,9 +13,11 @@ const managerClient = HttpClient.extend({
 })
 
 export class Manager {
+  static confirmationRetries = 0
+
   static confirmQuery = `
-    mutation ConfirmSelf($token: String!, $port: Int!) {
-      confirmWorker(token: $token, port: $port)
+    mutation ConfirmSelf($token: String!) {
+      confirmWorker(token: $token)
     }
   `
 
@@ -27,12 +29,16 @@ export class Manager {
     >("graphql", {
       json: {
         query: this.confirmQuery,
-        variables: { token: CONFIG.TOKEN, port: CONFIG.PORT },
+        variables: { token: CONFIG.TOKEN },
       },
       throwHttpErrors: false,
       retry: {
-        limit: 720,
-        calculateDelay: () => 5000,
+        limit: 30,
+        calculateDelay: () => {
+          this.confirmationRetries++
+
+          return Math.min(this.confirmationRetries * 5000, 1000 * 60 * 10)
+        },
         methods: ["POST"],
         errorCodes: ["ETIMEDOUT", "ECONNRESET", "ECONNREFUSED"],
       },
